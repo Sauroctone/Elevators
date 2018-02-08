b40e4ec165cee31adacc7fd9eeb0c026b2213f52
Sauroctone <neo.laq.33@gmail.com>
2018-02-08 18:12:56 +0100

    controller joueur 1 fonctionnel

---
+++ Assets/Scripts/CardBehaviour.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBehaviour : MonoBehaviour {
+
+    
+    [Header("Nature")]
+
+    public Cards cardNature;
+    public CardController player;
+    
+    [Header("Selection")]
+
+    public AnimationCurve offsetCurve;
+    public float offsetTime;
+    public float offsetY;
+    public float originY;
+    public float scaleFactor;
+    Vector3 originScale;
+    Vector3 newScale;
+    public bool isSelected;
+    public Material hoverMat;
+    Material originMat;
+
+    [Header("Movement")]
+
+    public AnimationCurve moveCurve;
+    public float moveTime;
+
+    [Header("References")]
+    public Renderer rend;
+
+    void Start()
+    {
+        player.cardDico.Add(cardNature, this);
+        originY = transform.position.y;
+        originScale = transform.localScale;
+        originMat = rend.material;
+        newScale = new Vector3(originScale.x * scaleFactor, originScale.y, originScale.z * scaleFactor);
+    }
+
+    public void Hovered()
+    {
+        rend.material = hoverMat;
+    }
+
+    public void NotHovered()
+    {
+        rend.material = originMat;
+    }
+
+	public void Select()
+    {
+        StartCoroutine(SelectCor());
+    }
+
+    public void Deselect()
+    {
+        StartCoroutine(DeselectCor()); 
+    }
+
+    public void Move(Vector3 _newPos)
+    {
+        StartCoroutine(MoveCor(_newPos));
+    }
+
+    IEnumerator SelectCor()
+    {
+        isSelected = true;
+
+        Vector3 pos = transform.position;
+        float time = 0f;
+        while(time < offsetTime)
+        {
+            transform.position = Vector3.Lerp(pos, new Vector3(pos.x, offsetY, pos.z), offsetCurve.Evaluate(time/offsetTime));
+            transform.localScale = Vector3.Lerp(originScale, newScale, offsetCurve.Evaluate(time / offsetTime));
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = new Vector3(pos.x, offsetY, pos.z);
+        transform.localScale = newScale;
+    }
+
+    IEnumerator DeselectCor()
+    {
+        isSelected = false;
+
+        Vector3 pos = transform.position;
+        float time = 0f;
+        while (time < offsetTime)
+        {
+            transform.position = Vector3.Lerp(pos, new Vector3(pos.x, originY, pos.z), offsetCurve.Evaluate(time / offsetTime));
+            transform.localScale = Vector3.Lerp(newScale, originScale, offsetCurve.Evaluate(time / offsetTime));
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = new Vector3(pos.x, originY, pos.z);
+        transform.localScale = originScale;
+    }
+
+    IEnumerator MoveCor(Vector3 _newPos)
+    {
+        Vector3 firstPos = transform.position;
+
+        float time = 0f;
+        while (time < moveTime)
+        {
+            transform.position = Vector3.Lerp(firstPos, _newPos, moveCurve.Evaluate(time / moveTime));
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = _newPos;
+    }
+}
--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -4,82 +4,268 @@ using UnityEngine;
 
 public class CardController : MonoBehaviour {
 
-    public List<Transform> cardList = new List<Transform>();
+    /*
+    [System.Serializable]
+    public struct PlayerCard
+    {
+        public Cards cardType;
+        public CardBehaviour behaviour;
+    }
+
+    public List<PlayerCard> playerCards = new List<PlayerCard>();
+    */
+
+    public Dictionary<Cards, CardBehaviour> cardDico = new Dictionary<Cards, CardBehaviour>();
+    public List<CardBehaviour> cardOrder = new List<CardBehaviour>();
+
     [Range(1, 2)]
     public int player;
-    string inputSelect;
-    string inputHorizontal;
+    public string attack;
+    public string shield;
+    public string backstep;
     bool canChangeCards = true;
-    public float horCooldown;
+    //public float horCooldown;
+
+    x360_Gamepad gamepad;
 
     [Header("References")]
     public GameManager gameMan;
-    Transform selectedCard;
-    Transform hoveredCard;
+    GamepadManager gamepadMan;
+    CardBehaviour selectedCard;
+  //  CardBehaviour hoveredCard;
 
     void Start()
     {
-        hoveredCard = cardList[0];
-
-        if (player == 1)
+        //hoveredCard = cardDico[0];
+        //       hoveredCard.Hovered();
+/*
+        Dictionary<Cards, CardBehaviour>.ValueCollection valueColl = cardDico.Values;
+        foreach (CardBehaviour card in valueColl)
         {
-            inputSelect = gameMan.playerOneInputs[0];
-            inputHorizontal = gameMan.playerOneInputs[1];
+            cardOrder.Add(card);
         }
-
-        else
-        {
-            inputSelect = gameMan.playerTwoInputs[0];
-            inputHorizontal = gameMan.playerTwoInputs[1];
-        }
+*/
+        gamepadMan = GamepadManager.Instance;
+        gamepad = gamepadMan.GetGamepad(player);
     }
 
     void Update()
     {
         if (canChangeCards)
         {
-            if (Input.GetAxisRaw(inputHorizontal) == -1)
+            SelectCard();
+
+            if (selectedCard != null)
             {
-                int cardIndex = cardList.IndexOf(hoveredCard) - 1;
-
-                if (cardIndex < 0)
-                {
-                    hoveredCard = cardList[cardList.Count - 1];
-                }
-
-                else
-                    hoveredCard = cardList[cardIndex];
-
-                print(hoveredCard.name);
-
-                canChangeCards = false;
-                StartCoroutine(HorizontalCooldown());
-            }
-
-            if (Input.GetAxisRaw(inputHorizontal) == 1)
-            {
-                int cardIndex = cardList.IndexOf(hoveredCard) + 1;
-
-                if (cardIndex == cardList.Count)
-                {
-                    hoveredCard = cardList[0];
-                }
-
-                else
-                    hoveredCard = cardList[cardIndex];
-
-                print(hoveredCard.name);
-
-                canChangeCards = false;
-                StartCoroutine(HorizontalCooldown());
+                MoveCard();
             }
         }
     }
 
-    IEnumerator HorizontalCooldown()
+    void SelectCard()
     {
-        yield return new WaitForSeconds(horCooldown);
-        canChangeCards = true;
+        if (gamepad.GetButtonDown(attack))
+        {
+            if (selectedCard != null)
+            {
+                selectedCard.Deselect();
+            }
+
+            if (selectedCard != null && selectedCard.cardNature == Cards.Attack)
+            {
+                selectedCard = null;
+            }
+
+            else
+            {
+                selectedCard = cardDico[Cards.Attack];
+                selectedCard.Select();
+            }
+        }
+
+        if (gamepad.GetButtonDown(shield))
+        {
+            if (selectedCard != null)
+            {
+                selectedCard.Deselect();
+            }
+
+            if (selectedCard != null && selectedCard.cardNature == Cards.Shield)
+            {
+                selectedCard = null;
+            }
+
+            else
+            {
+                selectedCard = cardDico[Cards.Shield];
+                selectedCard.Select();
+            }
+        }
+
+        if (gamepad.GetButtonDown(backstep))
+        {
+            if (selectedCard != null)
+            {
+                selectedCard.Deselect();
+            }
+
+            if (selectedCard != null && selectedCard.cardNature == Cards.Backstep)
+            {
+                selectedCard = null;
+            }
+
+            else
+            {
+                selectedCard = cardDico[Cards.Backstep];
+                selectedCard.Select();
+            }
+        }
     }
 
+    /* void HoverOverCards()
+    {
+        if (gamepad.GetStick_L().X == -1)
+        {
+            hoveredCard.NotHovered();
+
+            int cardIndex = cardList.IndexOf(hoveredCard) - 1;
+
+            if (cardIndex < 0)
+            {
+                hoveredCard = cardList[cardList.Count - 1];
+            }
+
+            else
+                hoveredCard = cardList[cardIndex];
+
+            hoveredCard.Hovered();
+
+            canChangeCards = false;
+            StartCoroutine(HorizontalCooldown()); 
+        }
+
+        if (gamepad.GetStick_L().X == 1)
+        {
+            hoveredCard.NotHovered();
+
+            int cardIndex = cardList.IndexOf(hoveredCard) + 1;
+
+            if (cardIndex == cardList.Count)
+            {
+                hoveredCard = cardList[0];
+            }
+
+            else
+                hoveredCard = cardList[cardIndex];
+
+            hoveredCard.Hovered();
+
+            canChangeCards = false;
+            StartCoroutine(HorizontalCooldown());
+        }
+    }
+    */
+
+    void MoveCard()
+    {
+        if (gamepad.GetStick_L().X == 1)
+        {
+            //Get card indexes
+            int cardIndex = cardOrder.IndexOf(selectedCard);
+            int otherCardIndex = cardIndex + 1;
+
+            if (otherCardIndex == cardOrder.Count)
+            {
+                otherCardIndex = 0;
+            }
+
+            //Get other card
+            CardBehaviour otherCard = cardOrder[otherCardIndex];
+            CardBehaviour middleCard = null;
+            if (otherCardIndex == 0)
+            {
+                middleCard = cardOrder[1];
+            }
+
+            //Swap card positions
+            selectedCard.Move(new Vector3 (otherCard.transform.position.x, selectedCard.offsetY, otherCard.transform.position.z));
+
+            if (middleCard != null)
+            {
+                otherCard.Move(new Vector3(middleCard.transform.position.x, middleCard.originY, middleCard.transform.position.z));
+                middleCard.Move(new Vector3(selectedCard.transform.position.x, selectedCard.originY, selectedCard.transform.position.z));
+
+                //Update card order
+                cardOrder[1] = otherCard;
+                cardOrder[otherCardIndex] = selectedCard;
+                cardOrder[cardIndex] = middleCard;
+            }
+
+            else
+            {
+                otherCard.Move(new Vector3(selectedCard.transform.position.x, selectedCard.originY, selectedCard.transform.position.z));
+
+                //Update card order
+                cardOrder[cardIndex] = otherCard;
+                cardOrder[otherCardIndex] = selectedCard;
+            }
+
+            //Input cooldown
+            canChangeCards = false;
+            StartCoroutine(HorizontalCooldown(selectedCard.moveTime));
+        }
+
+        if (gamepad.GetStick_L().X == -1)
+        {
+            //Get card indexes
+            int cardIndex = cardOrder.IndexOf(selectedCard);
+            int otherCardIndex = cardIndex - 1;
+
+            if (otherCardIndex < 0)
+            {
+                otherCardIndex = cardOrder.Count-1;
+            }
+
+            //Get other card
+            CardBehaviour otherCard = cardOrder[otherCardIndex];
+            CardBehaviour middleCard = null;
+            if (otherCardIndex == cardOrder.Count - 1)
+            {
+                middleCard = cardOrder[1];
+            }
+
+            //Swap card positions
+            selectedCard.Move(new Vector3(otherCard.transform.position.x, selectedCard.offsetY, otherCard.transform.position.z));
+
+            if (middleCard != null)
+            {
+                otherCard.Move(new Vector3(middleCard.transform.position.x, middleCard.originY, middleCard.transform.position.z));
+                middleCard.Move(new Vector3(selectedCard.transform.position.x, selectedCard.originY, selectedCard.transform.position.z));
+
+                //Update card order
+                cardOrder[1] = otherCard;
+                cardOrder[otherCardIndex] = selectedCard;
+                cardOrder[cardIndex] = middleCard;
+            }
+
+            else
+            {
+                otherCard.Move(new Vector3(selectedCard.transform.position.x, selectedCard.originY, selectedCard.transform.position.z));
+
+                //Update card order
+                cardOrder[cardIndex] = otherCard;
+                cardOrder[otherCardIndex] = selectedCard;
+            }
+
+            //Input cooldown
+            canChangeCards = false;
+            StartCoroutine(HorizontalCooldown(selectedCard.moveTime));
+        }
+    }
+
+    IEnumerator HorizontalCooldown(float _horCooldown)
+    {
+        yield return new WaitForSeconds(_horCooldown);
+        canChangeCards = true;
+    }
 }
